d0fff677644f8426e695a55b60ce07bcc478ce62
Dominick DeChristofaro <dominick.dechristofaro@gmail.com>
2019-09-10 15:20:48 -0400

    Edited POM, added mySQL version, created project folder structure

---
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/RentalStoreGroupApplication.java
@@ -0,0 +1,13 @@
+package com.company.rentalstoregroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalStoreGroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalStoreGroupApplication.class, args);
+	}
+
+}
