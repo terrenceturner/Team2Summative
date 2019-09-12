d3090a8a3ed5664e5f75b08e61ce1d109d70d390
terrenceturner <tsturner24@gmail.com>
2019-09-12 11:32:30 -0400

    completed item dto, dao, jdbc template

--- rental-store-group/src/main/java/com/company/rentalstoregroup/dao/ItemDao.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/dao/ItemDao.java
@@ -1,5 +1,19 @@
 package com.company.rentalstoregroup.dao;
 
+import com.company.rentalstoregroup.dto.Item;
+
+import java.util.List;
+
 public interface ItemDao {
+
     //DAO methods (no implementation code, i.e. Class addClass (Class class);)
+    Item getItem(int item_id);
+
+    List<Item> getAllItems();
+
+    Item addItem(Item item);
+
+    void updateItem(Item item);
+
+    void deleteItem(int id);
 }
