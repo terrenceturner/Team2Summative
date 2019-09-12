9f279183303bf8b1d6d790d9e35846e4d32ddda1
Dominick DeChristofaro <dominick.dechristofaro@gmail.com>
2019-09-12 15:58:04 -0400

    Stuff

--- rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -1,12 +1,9 @@
 package com.company.rentalstoregroup.dao;
-
-import com.company.rentalstoregroup.dto.Customer;
 import com.company.rentalstoregroup.dto.Invoice;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
@@ -16,7 +13,7 @@ public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao {
     //Prepared Statements
     private static final String INSERT_INVOICE_SQL = "INSERT INTO invoice (customer_id, order_date, pickup_date, return_date, late_fee values (?, ?, ?, ?, ?)";
     private static final String SELECT_INVOICE_SQL = "SELECT * FROM invoice WHERE invoice_id =  ?";
-    private static final String SELECT_ALL_INVOICES_SQL = "SELECT * FROM invoices";
+    private static final String SELECT_ALL_INVOICES_SQL = "SELECT * FROM invoice";
     private static final String UPDATE_INVOICE_SQL = "UPDATE invoice SET customer_id = ?, order_date = ?, pickup_date = ?, return_date = ?, late_fee = ? WHERE invoice_id = ?";
     private static final String DELETE_INVOICE_SQL = "DELETE FROM invoice WHERE invoice_id = ?";
     private static final String GET_INVOICE_BY_CUSTOMER_SQL = "GET * FROM invoice WHERE customer_id = ?";
