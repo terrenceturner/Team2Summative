9f6242b4ae9f105f5531a2d86a96292b8e05c574
BarbaraHernandez <bandezanne@gmail.com>
2019-09-12 13:38:35 -0400

    added base interface and implementation file

--- rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -1,15 +1,42 @@
 package com.company.rentalstoregroup.dao;
 
+import com.company.rentalstoregroup.dto.Invoice;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class InvoiceDaoJdbcTemplateImpl {
+public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao {
     //Prepared Statements
 
+    //Method Implementation
     private JdbcTemplate jdbcTemplate;
 
-    //Method Implementation
+    @Override
+    public Invoice addInvoice(Invoice invoice) {
+        return null;
+    }
+
+    @Override
+    public Invoice getInvoice(int id) {
+        return null;
+    }
+
+    @Override
+    public List<Invoice> getAllInvoices() {
+        return null;
+    }
+
+    @Override
+    public void updateInvoice(Invoice invoice) {
+
+    }
+
+    @Override
+    public void deleteInvoice(int id) {
+
+    }
 
     //row mapper
 }
