2001da42440af1e057c79748b2a997a24e66b472
BarbaraHernandez <bandezanne@gmail.com>
2019-09-13 11:20:41 -0400

    wrote create and delete in service layer for inventory view

--- rental-store-group/src/main/java/com/company/rentalstoregroup/service/ServiceLayer.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/service/ServiceLayer.java
@@ -4,8 +4,10 @@ import com.company.rentalstoregroup.dao.CustomerDao;
 import com.company.rentalstoregroup.dao.InvoiceDao;
 import com.company.rentalstoregroup.dao.Invoice_ItemDao;
 import com.company.rentalstoregroup.dao.ItemDao;
+import com.company.rentalstoregroup.dto.Invoice;
 import com.company.rentalstoregroup.dto.Invoice_Item;
 import com.company.rentalstoregroup.dto.Item;
+import com.company.rentalstoregroup.viewmodel.InvoiceViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -29,7 +31,40 @@ public class ServiceLayer {
     }
 
     //Invoice API
+    public InvoiceViewModel createInvoiceViewModel(InvoiceViewModel invoiceViewModel){
+        Invoice invoice = new Invoice(
+                invoiceViewModel.getCustomer_id(),
+                invoiceViewModel.getOrder_date(),
+                invoiceViewModel.getPickup_date(),
+                invoiceViewModel.getReturn_date(),
+                invoiceViewModel.getLate_fee()
+        );
 
+        invoice = invoiceDao.addInvoice(invoice);
+        invoiceViewModel.setInvoice_id(invoice.getInvoice_id());
+
+        List<Invoice_Item> invoice_list = invoiceViewModel.getInvoice_items();
+
+        invoice_list.forEach(i->
+        {
+            i.setInvoice_id(invoiceViewModel.getInvoice_id());
+            invoice_itemDao.addInvoice_Item(i);
+        });
+
+        List<Invoice_Item> db_list = invoice_itemDao.getInvoice_ItemByInvoice(invoiceViewModel.getInvoice_id());
+
+        invoiceViewModel.setInvoice_items(db_list);
+
+        return invoiceViewModel;
+    }
+
+    public void deleteInvoiceViewModel(int invoice_id){
+        List<Invoice_Item> invoice_itemList = invoice_itemDao.getInvoice_ItemByInvoice(invoice_id);
+
+        invoice_itemList.forEach(i -> invoice_itemDao.deleteInvoice_Item(i.getInvoice_item_id()));
+
+        invoiceDao.deleteInvoice(invoice_id);
+    }
 
 
     // Item API
--- rental-store-group/src/main/java/com/company/rentalstoregroup/viewmodel/InvoiceViewModel.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/viewmodel/InvoiceViewModel.java
@@ -1,4 +1,105 @@
 package com.company.rentalstoregroup.viewmodel;
 
+import com.company.rentalstoregroup.dto.Invoice_Item;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class InvoiceViewModel {
+    private int invoice_id;
+    private int customer_id;
+    private LocalDate order_date;
+    private LocalDate pickup_date;
+    private LocalDate return_date;
+    private BigDecimal late_fee;
+    private List<Invoice_Item> invoice_items = new ArrayList<>();
+
+    public int getInvoice_id() {
+        return invoice_id;
+    }
+
+    public void setInvoice_id(int invoice_id) {
+        this.invoice_id = invoice_id;
+    }
+
+    public int getCustomer_id() {
+        return customer_id;
+    }
+
+    public void setCustomer_id(int customer_id) {
+        this.customer_id = customer_id;
+    }
+
+    public LocalDate getOrder_date() {
+        return order_date;
+    }
+
+    public void setOrder_date(LocalDate order_date) {
+        this.order_date = order_date;
+    }
+
+    public LocalDate getPickup_date() {
+        return pickup_date;
+    }
+
+    public void setPickup_date(LocalDate pickup_date) {
+        this.pickup_date = pickup_date;
+    }
+
+    public LocalDate getReturn_date() {
+        return return_date;
+    }
+
+    public void setReturn_date(LocalDate return_date) {
+        this.return_date = return_date;
+    }
+
+    public BigDecimal getLate_fee() {
+        return late_fee;
+    }
+
+    public void setLate_fee(BigDecimal late_fee) {
+        this.late_fee = late_fee;
+    }
+
+    public List<Invoice_Item> getInvoice_items() {
+        return invoice_items;
+    }
+
+    public void setInvoice_items(List<Invoice_Item> invoice_items) {
+        this.invoice_items = invoice_items;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceViewModel that = (InvoiceViewModel) o;
+        return invoice_id == that.invoice_id &&
+                customer_id == that.customer_id &&
+                order_date.equals(that.order_date) &&
+                pickup_date.equals(that.pickup_date) &&
+                return_date.equals(that.return_date) &&
+                late_fee.equals(that.late_fee) &&
+                invoice_items.equals(that.invoice_items);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(invoice_id, customer_id, order_date, pickup_date, return_date, late_fee, invoice_items);
+    }
+
+    public InvoiceViewModel(int customer_id, LocalDate order_date, LocalDate pickup_date, LocalDate return_date, BigDecimal late_fee, List<Invoice_Item> invoice_items) {
+        this.customer_id = customer_id;
+        this.order_date = order_date;
+        this.pickup_date = pickup_date;
+        this.return_date = return_date;
+        this.late_fee = late_fee;
+        this.invoice_items = invoice_items;
+    }
+
+    public InvoiceViewModel(){}
 }
