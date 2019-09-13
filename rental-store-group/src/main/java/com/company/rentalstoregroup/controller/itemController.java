1d09f0f19f78fc023ee1cc1dbf2756c2f28dd968
terrenceturner <tsturner24@gmail.com>
2019-09-13 15:11:48 -0400

    completed itemController

--- rental-store-group/src/main/java/com/company/rentalstoregroup/controller/itemController.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/controller/itemController.java
@@ -1,6 +1,13 @@
 package com.company.rentalstoregroup.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.rentalstoregroup.dto.Item;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class itemController {
@@ -10,4 +17,54 @@ public class itemController {
     //@ResponseStatus(value = HttpStatus.*)
     //public void methodName(Class class){}
     //will call to service layer
+
+    private List<Item> itemList = new ArrayList<>();
+
+    //Post
+    @RequestMapping(value = "/item", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Item createItem(@RequestBody @Valid Item item){
+
+        itemList.add(item);
+
+        return item;
+
+    }
+
+    //Get
+    @RequestMapping(value = "/item/{itemId}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public Item getItem(@PathVariable int itemId){
+
+        for(Item item : itemList) {
+            if (Objects.equals(item.getItem_id(), itemId))
+                return item;
+        }
+
+        throw new IllegalArgumentException("Item not found.");
+    }
+
+    //Put
+    @RequestMapping(value = "/item/{itemId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void updateItem(@RequestBody @Valid Item item, @PathVariable int itemId){
+
+        if (itemId != item.getItem_id()){
+            throw new IllegalArgumentException("Item ID on path must match the ID in the item object");
+        }
+
+    }
+
+    //Delete
+    @RequestMapping(value = "/item/{itemId}", method = RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteItem(@PathVariable("itemId") int itemId){
+
+        int index = 0;
+        boolean found = true;
+
+        if (found)
+            itemList.remove(index);
+        else throw new IllegalArgumentException("Item not found.");
+    }
 }
