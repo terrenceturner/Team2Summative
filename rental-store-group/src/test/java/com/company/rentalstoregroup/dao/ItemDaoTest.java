9f9410ba037d4ada520c2cc34718ab3bcbb36615
terrenceturner <tsturner24@gmail.com>
2019-09-13 13:45:22 -0400

    fixed itemDaoTest

--- rental-store-group/src/test/java/com/company/rentalstoregroup/dao/ItemDaoTest.java
+++ rental-store-group/src/test/java/com/company/rentalstoregroup/dao/ItemDaoTest.java
@@ -64,11 +64,10 @@ public class ItemDaoTest {
     public void getAddItem() {
 
         Item item = new Item();
-        item.setItem_id(111);
         item.setName("Item");
         item.setDescription("Awesome Item");
         item.setDaily_rate(new BigDecimal("10.99"));
-        itemDao.addItem(item);
+        item = itemDao.addItem(item);
 
         Item item1 = itemDao.getItem(item.getItem_id());
 
@@ -80,11 +79,10 @@ public class ItemDaoTest {
     @Test
     public void deleteItem(){
         Item item = new Item();
-        item.setItem_id(111);
         item.setName("Item");
         item.setDescription("Awesome Item");
         item.setDaily_rate(new BigDecimal("10.99"));
-        itemDao.addItem(item);
+        item = itemDao.addItem(item);
 
         itemDao.deleteItem(item.getItem_id());
 
@@ -99,11 +97,10 @@ public class ItemDaoTest {
     public void updateItem(){
 
         Item item = new Item();
-        item.setItem_id(111);
         item.setName("Item");
         item.setDescription("Awesome Item");
         item.setDaily_rate(new BigDecimal("10.99"));
-        itemDao.addItem(item);
+        item = itemDao.addItem(item);
 
         item.setDescription("Nice Item");
         item.setDaily_rate(new BigDecimal("11.99"));
@@ -121,18 +118,16 @@ public class ItemDaoTest {
     public void getAllItems(){
 
         Item item = new Item();
-        item.setItem_id(111);
         item.setName("Item");
         item.setDescription("Awesome Item");
         item.setDaily_rate(new BigDecimal("10.99"));
-        itemDao.addItem(item);
+        item = itemDao.addItem(item);
 
         item = new Item();
-        item.setItem_id(222);
         item.setName("Other Item");
         item.setDescription("Nice Item");
         item.setDaily_rate(new BigDecimal("11.99"));
-        itemDao.addItem(item);
+        item = itemDao.addItem(item);
 
         List<Item> itemList = itemDao.getAllItems();
 
