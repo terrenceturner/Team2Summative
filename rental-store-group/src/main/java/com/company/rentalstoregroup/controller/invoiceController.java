d6bce66ce5df38bdd62ec9553dcabd09396501ac
Dominick DeChristofaro <dominick.dechristofaro@gmail.com>
2019-09-16 14:19:40 -0400

    All controllers working

--- rental-store-group/src/main/java/com/company/rentalstoregroup/controller/invoiceController.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/controller/invoiceController.java
@@ -1,22 +1,21 @@
 package com.company.rentalstoregroup.controller;
-
 import com.company.rentalstoregroup.service.ServiceLayer;
 import com.company.rentalstoregroup.viewmodel.InvoiceViewModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 public class invoiceController {
-    private ServiceLayer serviceLayer;
+    @Autowired
+    private ServiceLayer service;
 
     //create
-    @RequestMapping(value = "/invoice", method = RequestMethod.PUT)
+    @RequestMapping(value = "/invoice", method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
     public InvoiceViewModel createInvoice(@RequestBody InvoiceViewModel invoiceViewModel){
-        invoiceViewModel = serviceLayer.createInvoiceViewModel(invoiceViewModel);
-        return invoiceViewModel;
+        return service.createInvoiceViewModel(invoiceViewModel);
     }
 
 
@@ -24,14 +23,13 @@ public class invoiceController {
     @RequestMapping(value = "/invoice/{invoiceId}", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.OK)
     public void deleteInvoice(@PathVariable int invoiceId){
-        serviceLayer.deleteInvoiceViewModel(invoiceId);
+        service.deleteInvoiceViewModel(invoiceId);
     }
 
     //get invoices by customer id
     @RequestMapping(value = "/invoice/{customer_id}", method = RequestMethod.GET)
     @ResponseStatus(value = HttpStatus.FOUND)
-    public List<InvoiceViewModel> getInvoicesByCustomer(@PathVariable int customerId){
-        List<InvoiceViewModel> ivmList = serviceLayer.getInvoiceByCustomer(customerId);
-        return ivmList;
+    public List<InvoiceViewModel> getInvoicesByCustomer(@PathVariable int customer_id){
+        return service.getInvoiceByCustomer(customer_id);
     }
 }
--- rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/dao/InvoiceDaoJdbcTemplateImpl.java
@@ -17,7 +17,7 @@ public class InvoiceDaoJdbcTemplateImpl implements InvoiceDao {
     private static final String SELECT_ALL_INVOICES_SQL = "SELECT * FROM invoice";
     private static final String UPDATE_INVOICE_SQL = "UPDATE invoice SET customer_id = ?, order_date = ?, pickup_date = ?, return_date = ?, late_fee = ? WHERE invoice_id = ?";
     private static final String DELETE_INVOICE_SQL = "DELETE FROM invoice WHERE invoice_id = ?";
-    private static final String GET_INVOICE_BY_CUSTOMER_SQL = "GET * FROM invoice WHERE customer_id = ?";
+    private static final String GET_INVOICE_BY_CUSTOMER_SQL = "SELECT * FROM invoice WHERE customer_id = ?";
 
 
     //Method Implementation
