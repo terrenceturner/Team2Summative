2334bfa065bc8990938e5a26aed88be103db3227
Akshay Choudhary <ak92choudhary@gmail.com>
2019-09-16 11:20:10 -0400

    sending customer controller

--- rental-store-group/src/main/java/com/company/rentalstoregroup/controller/customerController.java
+++ rental-store-group/src/main/java/com/company/rentalstoregroup/controller/customerController.java
@@ -1,14 +1,46 @@
 package com.company.rentalstoregroup.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.company.rentalstoregroup.dto.Customer;
+import com.company.rentalstoregroup.service.ServiceLayer;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 public class customerController {
 
-    //HTTP methods
-    //@RequestMapping(value = "/", method = RequestMethod.*)
-    //@ResponseStatus(value = HttpStatus.*)
-    //public void methodName(Class class){}
+    private ServiceLayer service;
 
-    //will call to service layer
+    // Post
+    @RequestMapping(value = "/customer", method = RequestMethod.POST)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Customer addCustomer(@RequestBody @Valid Customer customer){
+        return service.saveCustomer(customer);
+    }
+
+    //Get
+
+    @RequestMapping(value = "/customers/{custId}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public Customer getCustomer(@PathVariable (name = "custId") int custId){
+        return service.findCustomer(custId);
+    }
+
+    //put
+    @RequestMapping(value = "/customers/{custId}", method = RequestMethod.PUT)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void updateCustomer(@RequestBody Customer customer, @PathVariable (name = "custId") int custId){
+        service.updateCustomer(customer);
+    }
+
+    //delete
+    @RequestMapping(value = "/customers/{custId}", method = RequestMethod.DELETE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+    public void deleteItem(@PathVariable(name = "custId") int custId){
+        service.removeItem(custId);
+    }
+
+
+
 }
